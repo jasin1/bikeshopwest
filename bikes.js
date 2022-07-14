ebd343a1fcdfd9e21709657d414355d1f0cd44fc
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-14 09:33:17 +0200

    const instead of let

--- bikes.js
+++ bikes.js
@@ -1,13 +1,13 @@
 console.log("We are in!!");
 
-let openBtns = document.querySelectorAll(".bike-img-wrapper");
+const openBtns = document.querySelectorAll(".bike-img-wrapper");
 let modalContainer = document.getElementById("bike-modal-wrapper");
 let modal = document.getElementById("modal-content");
 let closeBtn = document.getElementById("modal-close-wrapper");
 
 openBtns.forEach(btn =>{
   btn.addEventListener('click', function handleClick(event){
-    modalContainer.style.display = 'block';
+    //modalContainer.style.display = 'block';
     console.log('bike clicked', event);
   });
 
