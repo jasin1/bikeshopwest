281a65727523d49668f71b085715288872b23cb3
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-14 08:45:36 +0200

    Created modal functionality

--- bikes.js
+++ bikes.js
@@ -1,1 +1,20 @@
 console.log("We are in!!");
+
+let openBtn = document.getElementById("bike-img-wrapper");
+let modalContainer = document.getElementById("bike-modal-wrapper");
+let modal = document.getElementById("modal-content");
+let closeBtn = document.getElementById("modal-close-wrapper");
+
+openBtn.addEventListener('click', function(){
+  modalContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function(){
+ modalContainer.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+  if(e.target === modalContainer){
+    modalContainer.style.display = 'none';
+  }
+});
