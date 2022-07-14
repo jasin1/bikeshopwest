8fc59c59f427b1739bb4ac1e588d8dd40d04086d
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-14 09:18:12 +0200

    adding eventListener to openBtns childeren

--- bikes.js
+++ bikes.js
@@ -1,13 +1,17 @@
 console.log("We are in!!");
 
-let openBtn = document.getElementsByClassName(".bike-img-wrapper");
+let openBtns = document.getElementsByClassName(".bike-img-wrapper");
 let modalContainer = document.getElementById("bike-modal-wrapper");
 let modal = document.getElementById("modal-content");
 let closeBtn = document.getElementById("modal-close-wrapper");
 
-openBtn.addEventListener('click', function(){
+for (let i = 0; i < openBtns.length; i++){
+
+openBtns[i].addEventListener('click', function(){
   modalContainer.style.display = 'block';
 });
+
+}
 
 closeBtn.addEventListener('click', function(){
  modalContainer.style.display = 'none';
