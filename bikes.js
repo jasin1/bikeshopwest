e928eea1650839d35724d5af01d93872d358f6c5
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-14 09:39:05 +0200

    logging openButtons

--- bikes.js
+++ bikes.js
@@ -1,17 +1,19 @@
 console.log("We are in!!");
 
-const openBtns = document.querySelectorAll(".bike-img-wrapper");
+const openButtons = document.querySelectorAll(".bike-img-wrapper");
 let modalContainer = document.getElementById("bike-modal-wrapper");
 let modal = document.getElementById("modal-content");
 let closeBtn = document.getElementById("modal-close-wrapper");
 
-openBtns.forEach(btn =>{
-  btn.addEventListener('click', function handleClick(event){
-    //modalContainer.style.display = 'block';
-    console.log('bike clicked', event);
-  });
+console.log(openButtons);
 
-});
+// openButtons.forEach(btn => {
+//   btn.addEventListener('click', function handleClick(event){
+//     //modalContainer.style.display = 'block';
+//     console.log('bike clicked', event);
+//   });
+
+// });
 
 
 
