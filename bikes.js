e8c8561717fd7e89ea0fb55db9bdadd68bc2b640
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-14 09:19:34 +0200

    added console log to check if clicks are happening

--- bikes.js
+++ bikes.js
@@ -9,6 +9,7 @@ for (let i = 0; i < openBtns.length; i++){
 
 openBtns[i].addEventListener('click', function(){
   modalContainer.style.display = 'block';
+  console.log('bike clicked');
 });
 
 }
@@ -20,5 +21,6 @@ closeBtn.addEventListener('click', function(){
 window.addEventListener('click', function(e){
   if(e.target === modalContainer){
     modalContainer.style.display = 'none';
+    console.log('Modal clicked');
   }
 });
