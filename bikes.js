1aa299faccbc99334ed61a4477d1b14ba4c45dce
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-15 14:49:10 +0200

    disabled the steps, it takes to long via github

--- bikes.js
+++ bikes.js
@@ -39,33 +39,33 @@ window.addEventListener('click', function(e){
 
 //------------ Tabs---------------
 
-const multiStepForm = document.querySelector("[data-multi-step]");
-const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
+// const multiStepForm = document.querySelector("[data-multi-step]");
+// const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
 
-let currentStep = formSteps.findIndex(step =>{
-  return step.classList.contains("active")
-});
+// let currentStep = formSteps.findIndex(step =>{
+//   return step.classList.contains("active")
+// });
 
-if(currentStep < 0){
-  currentStep = 0;
-  // formSteps[currentStep].classList.add("active");
-  showCurrentStep();
-};
+// if(currentStep < 0){
+//   currentStep = 0;
+//   // formSteps[currentStep].classList.add("active");
+//   showCurrentStep();
+// };
 
-//console.log(currentStep);
+// //console.log(currentStep);
 
-multiStepForm.addEventListener("click", e => {
-  if(e.target.matches("[data-next")){
-    currentStep += 1;
-  } else if(e.target.matches("[data-prev]")){
-    currentStep -= 1;
-  }
-  showCurrentStep();
-})
+// multiStepForm.addEventListener("click", e => {
+//   if(e.target.matches("[data-next")){
+//     currentStep += 1;
+//   } else if(e.target.matches("[data-prev]")){
+//     currentStep -= 1;
+//   }
+//   showCurrentStep();
+// })
 
-function showCurrentStep(){
-  formSteps.forEach((step, index) => {
-    step.classList.toggle("active", index === currentStep);
-    console.log("stepping");
-  })
-}
+// function showCurrentStep(){
+//   formSteps.forEach((step, index) => {
+//     step.classList.toggle("active", index === currentStep);
+//    // console.log("stepping");
+//   })
+// }
