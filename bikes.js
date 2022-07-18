69a6ec753835c3d3b1537c8bd0941ddc4059b270
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-18 11:22:43 +0200

    modal image innerHTML

--- bikes.js
+++ bikes.js
@@ -72,7 +72,7 @@ for (let i =0; i < bikeBtns.length; i++){
     document.body.style.overflow = 'hidden';
     bikeHeading.innerHTML = bikeItemsData[i].name;
     modalTxt.innerHTML = bikeItemsData[i].descLong;
-    modalImg = bikeItemsData[i].img;
+    modalImg.innerHTML = bikeItemsData[i].img;
     console.log(modalImg);
   })
 };
