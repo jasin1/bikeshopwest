5cc6013854f3017528af7d9af7baaa702f0ac4b8
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-19 16:25:36 +0200

    updating hidden dateInput

--- bikes.js
+++ bikes.js
@@ -210,7 +210,7 @@ config = {
       let freshDate = chosenDate.toString();
       newDatum = freshDate.substring(0,21);
       stepTwoDate.innerText = newDatum;
-      dateCollected.value = newDatum;
+      dateCollected.innerText = newDatum;
       console.log(newDatum);     
   }
 };
