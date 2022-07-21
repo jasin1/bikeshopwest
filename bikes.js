bdd4d32fc33afb3ba4579f1a14af5f3b385b0f7b
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-07-21 11:02:51 +0200

    where is my 1 day`/

--- bikes.js
+++ bikes.js
@@ -193,7 +193,7 @@ let selectedDayValue = selectedDays.value;
 //priceCollected
 
 selectedDays.addEventListener('change', function(){
-  if(selectedDayValue = selectedDays[1].value){
+  if(selectedDayValue == 1){
   console.log("ONE day");    
   }else{
     console.log("more days");    
