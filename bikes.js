1c7b87ce297956feb71a2fe755ea326fc7a70218
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-08-03 17:45:07 +0200

    ok, nu dan

--- bikes.js
+++ bikes.js
@@ -296,7 +296,7 @@ const addOnPrices = [addOnPrice1, addOnPrice2, addOnPrice3];
         totalVal -= val;
         console.log("totalVal is ", totalVal);
         if(bigTotal>0){
-        totalPrice.innerText = bigTotal - parseInt(check.value);
+        totalPrice.innerText = bigTotal - totalVal;
         } 
         //priceCollected.value = bigTotal - val;             
       }
