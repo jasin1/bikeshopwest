d3484fe0b50ca93858e0dcdadcf399ebcc71e89f
jasin1 <jasin.tairaidrissi@gmail.com>
2022-08-10 15:26:35 +0200

    Removed some commented lines

--- bikes.js
+++ bikes.js
@@ -344,57 +344,7 @@ function addingCalcs(calc1, calc2){
     }
    
 });
-
-// totalPrice.innerText = addingCalcs(bigTotal, totalVal);
-
-//------------------------ Eerste berekening -------------------------------------------------
-
-//   checks.forEach(check =>{
-//     check.addEventListener("change",()=>{
-//       val = parseInt(check.value);
-//       if(check.checked){
-//           console.log("btn is checked");
-//           totalVal += val;
-//           totalPrice.innerText = bigTotal + totalVal;
-//           console.log("totalVal is ", totalVal);
-//       } else{
-//         console.log("btn is unchecked");
-//         totalVal -= val;
-//         console.log("totalVal is ", totalVal);
-//         totalPrice.innerText = bigTotal - totalVal;          
-//       }
-//     })
-
-//   })
-
-// //---------------------------------------------------------------------
-
-
-// selectedDays.addEventListener('change', function(){
-//   theValue = parseInt(selectedDays.value);
-//   step2duration.innerText = theValue + " days";
-//   if(theValue === 1){
-//     step2duration.innerText = theValue + " day";
-//     bigTotal = (theValue * bikeItemsData[selectedBikeNum].price1);
-//     totalPrice.innerText = bigTotal;
-//     totalPrice2.innerText = bigTotal;
-//     priceCollected.value = bigTotal;
-//     console.log(bigTotal);
-//   }else{
-//     bigTotal = (theValue * bikeItemsData[selectedBikeNum].price2);
-//     totalPrice.innerText = bigTotal;
-//     totalPrice2.innerText = bigTotal;
-//     priceCollected.value = bigTotal;
-//     console.log(bigTotal);      
-//   }
-//   console.log("The selected value is " , theValue);
-// });
-
-
-
-
-
-
+ 
 //---------------- FlatPickr---------------------------------------
 
 // let chosenDate = "";
