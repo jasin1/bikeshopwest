e8a25d880be61efd25c5f66a5309062fae1c6fe0
jasin1 <jasin.tairaidrissi@gmail.com>
2022-12-28 22:21:21 +0100

    You have to keep trying

--- flatpickr-practice.js
+++ flatpickr-practice.js
@@ -1,49 +1,80 @@
+flatpickr("#input-date", {
+  onChange: function(selectedDates, dateStr, instance) {
+    // this function will be called every time the user selects a new date
+    updateTimeField(dateStr);  // call the updateTimeField function with the selected date
+  }
+});
 
-  // Create a Flatpickr instance for the #input-date element
-  var inputDate = flatpickr('#input-date', {
-    // Set the onChange option to a function that updates the #input-time element
-    onChange: function(selectedDates, dateStr, instance) {
-      // Get the value of the selected day
-      var selectedDay = dateStr.split(' ')[0];
+function updateTimeField(dateStr) {
+  // get the day of the week for the selected date
+  var day = new Date(dateStr).getDay();
 
-      // Update the enableTime and time_24hr options of the #input-time element's Flatpickr instance
-      switch(selectedDay) {
-        case 'Monday':
-          inputTime.set('enableTime', true);
-          inputTime.set('time_24hr', true);
-          inputTime.set('minTime', '14:00');
-          inputTime.set('maxTime', '17:45');
-          break;
-        case 'Friday':
-          inputTime.set('enableTime', true);
-          inputTime.set('time_24hr', true);
-          inputTime.set('minTime', '15:00');
-          inputTime.set('maxTime', '17:45');
-          break;
-        case 'Saturday':
-          inputTime.set('enableTime', true);
-          inputTime.set('time_24hr', true);
-          inputTime.set('minTime', '10:00');
-          inputTime.set('maxTime', '16:45');
-          break;
-        case 'Sunday':
-          inputTime.set('enableTime', false);
-          break;
-        default:
-          inputTime.set('enableTime', true);
-          inputTime.set('time_24hr', true);
-          inputTime.set('minTime', '10:00');
-          inputTime.set('maxTime', '17:45');
-      }
-    }
-  });
+  // determine the open and closing times for the selected day
+  var openTime, closingTime;
+  switch (day) {
+    case 1:  // Monday
+      openTime = "14:00";
+      closingTime = "17:45";
+      break;
+    case 5:  // Friday
+      openTime = "15:00";
+      closingTime = "17:45";
+      break;
+    case 6:  // Saturday
+      openTime = "10:00";
+      closingTime = "16:45";
+      break;
+    case 0:  // Sunday
+      openTime = "";
+      closingTime = "";
+      break;
+    default:  // all other days
+      openTime = "10:00";
+      closingTime = "17:45";
+  }
 
-  // Create a Flatpickr instance for the #input-time element
-  var inputTime = flatpickr('#input-time', {
-    enableTime: true,
-    time_24hr: true,
-    minTime: '10:00',
-    maxTime: '17:45'
-  });
+  // set the value of the "input-time" field to the open time
+  document.getElementById("input-time").value = openTime;
 
-  
+  // enable or disable the "input-time" field based on whether the shop is open or closed
+  document.getElementById("input-time").disabled = (openTime === "");
+}
+
+function updateTimeField(dateStr) {
+  // get the day of the week for the selected date
+  var day = new Date(dateStr).getDay();
+
+  // determine the open and closing times for the selected day
+  var openTime, closingTime;
+  switch (day) {
+    case 1:  // Monday
+      openTime = "14:00";
+      closingTime = "17:45";
+      break;
+    case 5:  // Friday
+      openTime = "15:00";
+      closingTime = "17:45";
+      break;
+    case 6:  // Saturday
+      openTime = "10:00";
+      closingTime = "16:45";
+      break;
+    case 0:  // Sunday
+      openTime = "";
+      closingTime = "";
+      break;
+    default:  // all other days
+      openTime = "10:00";
+      closingTime = "17:45";
+  }
+
+  // set the value of the "input-time" field to the open time
+  document.getElementById("input-time").value = openTime;
+
+  // set the min and max attributes of the "input-time" field based on the open and closing times
+  document.getElementById("input-time").min = openTime;
+  document.getElementById("input-time").max = closingTime;
+
+  // enable or disable the "input-time" field based on whether the shop is open or closed
+  document.getElementById("input-time").disabled = (openTime === "");
+}
