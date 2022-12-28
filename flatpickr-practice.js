8fc3a818200389a2d41dc27b9fd88fdfdda51333
jasin1 <jasin.tairaidrissi@gmail.com>
2022-12-28 23:45:41 +0100

    yeah, why not

--- flatpickr-practice.js
+++ flatpickr-practice.js
@@ -1,53 +1,45 @@
-const inputDate = document.querySelector('#input-date');
-const inputTime = document.querySelector('#input-time');
-
-flatpickr(inputDate, {
-  disable: [
-    (date) => date.getDay() === 0, // disable Sundays
-  ],
-});
-
-
-//inputDate.addEventListener
-
-// Define the opening and closing times for each day of the week
-const openingHours = {
+const openingTimes = {
   Monday: { open: "14:00", close: "17:45" },
   Tuesday: { open: "10:00", close: "17:45" },
   Wednesday: { open: "10:00", close: "17:45" },
   Thursday: { open: "10:00", close: "17:45" },
   Friday: { open: "15:00", close: "17:45" },
   Saturday: { open: "10:00", close: "16:45" },
-  Sunday: { open: "", close: "" } // Closed on Sunday
+  Sunday: { open: "", close: "" }
 };
 
-// Bind an event listener to the #input-date element
-inputDate.addEventListener("change", function(event) {
-  // Get the selected date
-  const selectedDate = event.target.value;
-  console.log(selectedDate); // Check the value of selectedDate
-  // Get the opening and closing times for the selected date
-  const { open, close } = openingHours[selectedDate];
 
-  // Get the #input-time dropdown element
-  const inputTime = document.querySelector("#input-time");
-
-  // Remove all existing options from the dropdown
-  inputTime.innerHTML = "";
-
-  // Calculate the number of 30-minute increments between the opening and closing times
-  const startTime = new Date("1970-01-01 " + open + ":00");
-  const endTime = new Date("1970-01-01 " + close + ":00");
-  const increments = (endTime - startTime) / (1000 * 60 * 30);
-
-  // Add a new option for each 30-minute increment
-  for (let i = 0; i <= increments; i++) {
-    const option = document.createElement("option");
-    option.value = startTime.toTimeString().substring(0, 5);
-    option.text = option.value;
-    inputTime.add(option);
-
-    // Increment the start time by 30 minutes
+function getAvailableTimes(day) {
+  const { open, close } = openingTimes[day];
+  let startTime = new Date("1970-01-01 " + open);
+  let endTime = new Date("1970-01-01 " + close);
+  let availableTimes = [];
+  while (startTime <= endTime) {
+    availableTimes.push(startTime.toTimeString().substring(0, 5));
     startTime.setMinutes(startTime.getMinutes() + 30);
+  }
+  return availableTimes;
+}
+
+
+flatpickr("#input-date", {
+  onChange: function(selectedDates, dateStr, instance) {
+    // Get the day of the week for the selected date
+    let dayOfWeek = new Date(dateStr).toLocaleString("en-US", { weekday: "long" });
+
+    // Get the available times for the selected day
+    let availableTimes = getAvailableTimes(dayOfWeek);
+
+    // Clear the current options in the #input-time dropdown
+    let inputTime = document.querySelector("#input-time");
+    inputTime.innerHTML = "";
+
+    // Add the available times as options in the #input-time dropdown
+    availableTimes.forEach(time => {
+      let option = document.createElement("option");
+      option.value = time;
+      option.text = time;
+      inputTime.add(option);
+    });
   }
 });
