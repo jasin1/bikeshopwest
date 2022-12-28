a780e900772234f526faae42c390bc89b5227a8e
jasin1 <jasin.tairaidrissi@gmail.com>
2022-12-28 16:20:58 +0100

    helemaal kaal

--- flatpickr-practice.js
+++ flatpickr-practice.js
@@ -1,41 +1,68 @@
-// Define the opening times for each day of the week
+const inputDate = document.querySelector('.input-date');
+const inputTime = document.querySelector('.input-time');
+
+// An object containing the opening and closing times for each day of the week
 const openingTimes = {
-  "Monday": { "open": "14:00", "close": "17:45" },
-  "Tuesday": { "open": "10:00", "close": "17:45" },
-  "Wednesday": { "open": "10:00", "close": "17:45" },  
-  "Thursday": { "open": "10:00", "close": "17:45" }, 
-  "Friday": { "open": "15:00", "close": "17:45" },
-  "Saturday": { "open": "10:00", "close": "16:45" },
-  "Sunday": { "open": null, "close": null },
-  // other days of the week
+  Monday: {
+    open: '14:00',
+    close: '17:45',
+  },
+  Friday: {
+    open: '15:00',
+    close: '17:45',
+  },
+  Saturday: {
+    open: '10:00',
+    close: '16:45',
+  },
+  // Set the default opening times for the rest of the days
+  default: {
+    open: '10:00',
+    close: '17:45',
+  },
 };
 
-// Initialize the date picker for the "input-date" element
-const datePicker = new Pikaday({
-  field: document.getElementById('input-date'),
-  format: 'YYYY-MM-DD',
+// Listen for changes in the input-date field
+inputDate.addEventListener('change', () => {
+  // Get the selected day
+  const selectedDay = inputDate.value;
+
+  // Get the opening and closing times for the selected day
+  let { open, close } = openingTimes[selectedDay] || openingTimes.default;
+
+  // Set the minimum and maximum allowed times in the input-time field
+  inputTime.min = open;
+  inputTime.max = close;
+
+  // Update the options in the input-time field
+  updateTimeOptions(open, close);
 });
 
-// Initialize the time picker for the "input-time" element
-const timePicker = new Pikaday({
-  field: document.getElementById('input-time'),
-  format: 'HH:mm',
-  use24hour: true,
-});
+function updateTimeOptions(open, close) {
+  // Convert the opening and closing times to Date objects
+  const start = new Date('1970-01-01T' + open + 'Z');
+  const end = new Date('1970-01-01T' + close + 'Z');
 
-// Add an event listener to the "input-date" element to update the "input-time" element
-document.getElementById('input-date').addEventListener('change', function() {
-  // Get the selected date
-  const selectedDate = this.value;
-  // Check if the openingTimes object has a key for the selected date
-  if (openingTimes.hasOwnProperty(selectedDate)) {
-    // Get the opening and closing times for the selected day
-    const { open, close } = openingTimes[selectedDate];
-    // Check if the shop is open on the selected day
-    if (open && close) {
-      // Update the minTime and maxTime options of the time picker
-      timePicker.setMinTime(open);
-      timePicker.setMaxTime(close);
-    }
+  // Calculate the difference between the start and end times in minutes
+  const diffMinutes = (end - start) / (60 * 1000);
+
+  // Clear the current options in the input-time field
+  inputTime.innerHTML = '';
+
+  // Add a new option for each minute between the start and end times
+  for (let i = 0; i <= diffMinutes; i++) {
+    const option = document.createElement('option');
+    option.value = formatTime(new Date(start.getTime() + i * 60000));
+    option.text = option.value;
+    inputTime.appendChild(option);
   }
-});
+}
+
+// Format a Date object as a string in the HH:MM format
+function formatTime(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  return `${hours}:${minutes}`;
+}
