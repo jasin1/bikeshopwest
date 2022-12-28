b0f717bc4574a907ea01f0ebb3ff9d37f4afaa94
jasin1 <jasin.tairaidrissi@gmail.com>
2022-12-28 15:50:34 +0100

    using pikaday instead

--- flatpickr-practice.js
+++ flatpickr-practice.js
@@ -1,50 +1,30 @@
-const openingTimes = {
-  "Monday": { "open": "14:00", "close": "17:45" },
-  "Tuesday": { "open": "10:00", "close": "17:45" },
-  "Wednesday": { "open": "10:00", "close": "17:45" },  
-  "Thursday": { "open": "10:00", "close": "17:45" }, 
-  "Friday": { "open": "15:00", "close": "17:45" },
-  "Saturday": { "open": "10:00", "close": "16:45" },
-  "Sunday": { "open": null, "close": null },
-  // other days of the week
-}
-
-
-flatpickr('#input-date', {
-  dateFormat: "Y-m-d",
-  locale: {
-    firstDayOfWeek: 1,
-  }
+// Initialize the Pikaday date picker
+const datePicker = new Pikaday({
+  field: document.getElementById('input-date'),
+  format: 'YYYY-MM-DD',
+  firstDay: 1, // Set the first day of the week to Monday
 });
 
-flatpickr('#input-time', {
-  enableTime: true,
+// Initialize the time picker
+const timePicker = new Pikaday({
+  field: document.getElementById('input-time'),
+  format: 'HH:mm',
   noCalendar: true,
-  dateFormat: "H:i",
-  // other configuration options
 });
 
-document.getElementById('input-date').addEventListener('change', function() {
+// When the selected date changes, update the minTime and maxTime options of the time picker
+datePicker.on('change', function() {
   // Get the selected date
-  const selectedDate = this.value;
+  const selectedDate = this.toString();
   // Check if the openingTimes object has a key for the selected date
   if (openingTimes.hasOwnProperty(selectedDate)) {
     // Get the opening and closing times for the selected day
     const { open, close } = openingTimes[selectedDate];
     // Check if the shop is open on the selected day
     if (open && close) {
-      // Create Date objects from the opening and closing times
-      const minTime = new Date();
-      minTime.setHours(open.split(':')[0]);
-      minTime.setMinutes(open.split(':')[1]);
-      const maxTime = new Date();
-      maxTime.setHours(close.split(':')[0]);
-      maxTime.setMinutes(close.split(':')[1]);
-      // Update the minTime and maxTime options of the time input field    
-      flatpickr('#input-time', {
-        minTime: minTime,
-        maxTime: maxTime
-      });
+      // Update the minTime and maxTime options of the time picker
+      timePicker.setMinTime(open);
+      timePicker.setMaxTime(close);
     }
   }
 });
