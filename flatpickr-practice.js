dcfdde8ee00c631f6b9b6d832e5f0cc232ff85f4
jasin1 <jasin.tairaidrissi@gmail.com>
2022-12-28 15:01:06 +0100

    adding sunday to the mix

--- flatpickr-practice.js
+++ flatpickr-practice.js
@@ -1,3 +1,15 @@
+const openingTimes = {
+  "Monday": { "open": "14:00", "close": "17:45" },
+  "Tuesday": { "open": "10:00", "close": "17:45" },
+  "Wednesday": { "open": "10:00", "close": "17:45" },  
+  "Thursday": { "open": "10:00", "close": "17:45" }, 
+  "Friday": { "open": "15:00", "close": "17:45" },
+  "Saturday": { "open": "10:00", "close": "16:45" },
+  "Sunday": { "open": null, "close": null },
+  // other days of the week
+}
+
+
 flatpickr('.input-date', {
   // other configuration options
 });
@@ -10,30 +22,23 @@ flatpickr('.input-time', {
 });
 
 
-const openingTimes = {
-  "Monday": { "open": "14:00", "close": "17:45" },
-  "Tuesday": { "open": "10:00", "close": "17:45" },
-  "Wednesday": { "open": "10:00", "close": "17:45" },  
-  "Thursday": { "open": "10:00", "close": "17:45" }, 
-  "Friday": { "open": "15:00", "close": "17:45" },
-  "Saturday": { "open": "10:00", "close": "16:45" },
-  // other days of the week
-}
-
-document.querySelector('.input-date').addEventListener('change', function() {
+document.getElementById('input-date').addEventListener('change', function() {
   // Get the selected date
   const selectedDate = this.value;
   // Check if the openingTimes object has a key for the selected date
   if (openingTimes.hasOwnProperty(selectedDate)) {
     // Get the opening and closing times for the selected day
     const { open, close } = openingTimes[selectedDate];
-    // Create Date objects from the opening and closing times
-    const minTime = new Date(Date.parse(open));
-    const maxTime = new Date(Date.parse(close));
-    // Update the minTime and maxTime options of the time input field
-    flatpickr('.input-time', {
-      minTime: minTime,
-      maxTime: maxTime
-    });
+    // Check if the shop is open on the selected day
+    if (open && close) {
+      // Create Date objects from the opening and closing times
+      const minTime = new Date(Date.parse(open));
+      const maxTime = new Date(Date.parse(close));
+      // Update the minTime and maxTime options of the time input field
+      flatpickr('#input-time', {
+        minTime: minTime,
+        maxTime: maxTime
+      });
+    }
   }
 });
