892cb1c3722b5db78631d3360a3517bde9786a3b
jasin1 <jasin.tairaidrissi@gmail.com>
2022-12-28 23:28:43 +0100

    Dit is een latertje

--- flatpickr-practice.js
+++ flatpickr-practice.js
@@ -8,40 +8,46 @@ flatpickr(inputDate, {
 });
 
 
-inputDate.addEventListener('change', (event) => {
+//inputDate.addEventListener
+
+// Define the opening and closing times for each day of the week
+const openingHours = {
+  Monday: { open: "14:00", close: "17:45" },
+  Tuesday: { open: "10:00", close: "17:45" },
+  Wednesday: { open: "10:00", close: "17:45" },
+  Thursday: { open: "10:00", close: "17:45" },
+  Friday: { open: "15:00", close: "17:45" },
+  Saturday: { open: "10:00", close: "16:45" },
+  Sunday: { open: "", close: "" } // Closed on Sunday
+};
+
+// Bind an event listener to the #input-date element
+inputDate.addEventListener("change", function(event) {
+  // Get the selected date
   const selectedDate = event.target.value;
-  const dayOfWeek = new Date(selectedDate).getDay();
 
-  let openingTime;
-  let closingTime;
+  // Get the opening and closing times for the selected date
+  const { open, close } = openingHours[selectedDate];
 
-  switch (dayOfWeek) {
-    case 1: // Monday
-      openingTime = '14:00';
-      closingTime = '17:45';
-      break;
-    case 5: // Friday
-      openingTime = '15:00';
-      closingTime = '17:45';
-      break;
-    case 6: // Saturday
-      openingTime = '10:00';
-      closingTime = '16:45';
-      break;
-    case 0: // Sunday
-      // disable the inputTime element and exit the function
-      inputTime.setAttribute('disabled', 'disabled');
-      return;
-    default: // all other days
-      openingTime = '10:00';
-      closingTime = '17:45';
-      break;
+  // Get the #input-time dropdown element
+  const inputTime = document.querySelector("#input-time");
+
+  // Remove all existing options from the dropdown
+  inputTime.innerHTML = "";
+
+  // Calculate the number of 30-minute increments between the opening and closing times
+  const startTime = new Date("1970-01-01 " + open + ":00");
+  const endTime = new Date("1970-01-01 " + close + ":00");
+  const increments = (endTime - startTime) / (1000 * 60 * 30);
+
+  // Add a new option for each 30-minute increment
+  for (let i = 0; i <= increments; i++) {
+    const option = document.createElement("option");
+    option.value = startTime.toTimeString().substr(0, 5);
+    option.text = option.value;
+    inputTime.add(option);
+
+    // Increment the start time by 30 minutes
+    startTime.setMinutes(startTime.getMinutes() + 30);
   }
-
-  // update the default time for the inputTime element
-  inputTime.value = openingTime;
-
-  // set the minimum and maximum times that can be selected in the inputTime element
-  inputTime.setAttribute('min', openingTime);
-  inputTime.setAttribute('max', closingTime);
 });
