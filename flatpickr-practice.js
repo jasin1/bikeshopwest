2224663c80a67759f2c73bd6805e27c1a5efb5cc
jasin1 <jasin.tairaidrissi@gmail.com>
2022-12-28 09:37:23 +0100

    adding more stuff

--- flatpickr-practice.js
+++ flatpickr-practice.js
@@ -1,12 +1,63 @@
 console.log("flatpickr practice");
 
+//----- Modal variables---------------------------
+
+let openBtn1 = document.querySelector(".bike-img-wrapper-1");
+let openBtn2 = document.querySelector(".bike-img-wrapper-2");
+let openBtn3 = document.querySelector(".bike-img-wrapper-3");
+let openBtn4 = document.querySelector(".bike-img-wrapper-4");
+
+const bikeBtns = [openBtn1, openBtn2, openBtn3, openBtn4];
+
+const modalBookBtn = document.querySelector(".button-book-4");
+
+const prevBtn = document.querySelectorAll(".button-prev");
+const nextBtn = [...document.querySelectorAll(".button-next")];
+const formSteps = [...document.querySelectorAll(".tab")];
+
+const bookBtn1 = document.querySelector(".button-book-1");
+const bookBtn2 = document.querySelector(".button-book-2");
+const bookBtn3 = document.querySelector(".button-book-3");
+const bookBtn4 = document.querySelector(".button-book-new");
+
+const bookBtns = [bookBtn1, bookBtn2, bookBtn3, bookBtn4];
+
+const modalTxt = document.querySelector(".modal-txt-wrapper");
+const modalImg = document.querySelector(".modal-main-img");
+
+// --------- Steps variables ----------------------
+
+const bikeHeading = document.querySelector(".step-1-heading");
+const newHeading = document.querySelector(".new-selected-heading");
+const newHeading2 = document.querySelector(".new-selected-heading2");
+
+const stepTwoImg = document.querySelector(".step-2-img");
+const stepTwoDate = document.querySelector(".step-2-date");
+const stepThreeImg = document.querySelector(".step-3-img");
+
+const bikeSelectedImg = document.querySelector(".selected-bike-img");
+const bikePrice1 = document.querySelector(".step-1-price-1");
+const bikePrice2 = document.querySelector(".step-1-price-2");
+const totalPrice = document.querySelector(".total-price-calc");
+const totalPrice2 = document.querySelector(".total-price-calc-2");
+const selectedDate = document.querySelector(".step-2-date");
+
 //------------- Collecting inputs to send with form---------------------
 const selectedDays = document.getElementById("form-select-input");
 const dateCollected = document.querySelector(".date-colect-input");
 const priceCollected = document.querySelector(".price-colect-input");
 const timeCollected = document.querySelector(".time-colect-input");
 const DurationCollected = document.querySelector(".duration-input");
+//const BikeAmountCollected = document.querySelector(".bike-amount-input");
+//-----------------------------------------------------------------------
+const step2duration = document.querySelector(".step-2-duration");
+const step2Time = document.querySelector(".step-2-time");
+const step2Bikes = document.querySelector(".step-2-total-bikes");
+const checksWrapper = document.querySelector(".accessoires-select-wrapper");
 
+const rentalTop = document.querySelector(".rental-top-text");
+
+let toDay = "";
 
 //---------------- FlatPickr---------------------------------------
 
