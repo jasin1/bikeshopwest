f02a2e6cbe4516a356d8e1a2df9503a845a0f955
jasin1 <jasin.tairaidrissi@gmail.com>
2022-12-28 16:53:03 +0100

    it is date-fns

--- flatpickr-practice.js
+++ flatpickr-practice.js
@@ -1,74 +1,63 @@
-function init() {
- 
-  const inputDate = document.querySelector(".input-date");
-  const inputTime = document.querySelector(".input-time");
+// First, you'll need to include the date-fns library in your HTML file
+// You can do this by adding the following script tag to your HTML:
+// <script src="https://cdn.jsdelivr.net/npm/date-fns@2.16.1/dist/date-fns.min.js"></script>
 
-  // An object containing the opening and closing times for each day of the week
-  const openingTimes = {
-    Monday: {
-      open: "14:00",
-      close: "17:45",
-    },
-    Friday: {
-      open: "15:00",
-      close: "17:45",
-    },
-    Saturday: {
-      open: "10:00",
-      close: "16:45",
-    },
-    // Set the default opening times for the rest of the days
-    default: {
-      open: "10:00",
-      close: "17:45",
-    },
-  };
+// Set the default opening and closing times for each day of the week
+const defaultOpeningTimes = {
+  monday: {
+    opening: '14:00',
+    closing: '17:45',
+  },
+  tuesday: {
+    opening: '10:00',
+    closing: '17:45',
+  },
+  wednesday: {
+    opening: '10:00',
+    closing: '17:45',
+  },
+  thursday: {
+    opening: '10:00',
+    closing: '17:45',
+  },
+  friday: {
+    opening: '15:00',
+    closing: '17:45',
+  },
+  saturday: {
+    opening: '10:00',
+    closing: '16:45',
+  },
+  sunday: {
+    opening: '10:00',
+    closing: '17:45',
+  },
+};
 
-  // Listen for changes in the input-date field
-  inputDate.addEventListener("change", () => {
-    // Get the selected day
-    const selectedDay = inputDate.value;
+// Get a reference to the input fields
+const inputDate = document.querySelector('#input-date');
+const inputTime = document.querySelector('#input-time');
 
-    // Get the opening and closing times for the selected day
-    let { open, close } = openingTimes[selectedDay] || openingTimes.default;
+// Set the default value for the input time field to the opening time for Monday
+inputTime.value = defaultOpeningTimes.monday.opening;
 
-    // Set the minimum and maximum allowed times in the input-time field
-    inputTime.min = open;
-    inputTime.max = close;
+// Add an event listener to the input date field
+inputDate.addEventListener('change', (event) => {
+  // Get the selected day of the week
+  const selectedDate = new Date(event.target.value);
+  const selectedDay = getDay(selectedDate);
+  console.log(selectedDay);
 
-    // Update the options in the input-time field
-    updateTimeOptions(open, close);
-  });
+  // Get the opening and closing times for the selected day
+  const openingTime = defaultOpeningTimes[selectedDay].opening;
+  const closingTime = defaultOpeningTimes[selectedDay].closing;
+  console.log(openingTime, closingTime);
 
-  function updateTimeOptions(open, close) {
-    // Convert the opening and closing times to Date objects
-    const start = new Date("1970-01-01T" + open + "Z");
-    const end = new Date("1970-01-01T" + close + "Z");
+  // Update the value of the input time field
+  inputTime.value = openingTime;
 
-    // Calculate the difference between the start and end times in minutes
-    const diffMinutes = (end - start) / (60 * 1000);
-
-    // Clear the current options in the input-time field
-    inputTime.innerHTML = "";
-
-    // Add a new option for each minute between the start and end times
-    for (let i = 0; i <= diffMinutes; i++) {
-      const option = document.createElement("option");
-      option.value = formatTime(new Date(start.getTime() + i * 60000));
-      option.text = option.value;
-      inputTime.appendChild(option);
-    }
-  }
-
-  // Format a Date object as a string in the HH:MM format
-  function formatTime(date) {
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    return `${hours}:${minutes}`;
-  }
-}
-
-// Wait for the DOM to be ready before running the init function
-document.addEventListener('DOMContentLoaded', init);
+  // Set the min and max attributes for the input time field
+  // to restrict the range of times that can be selected
+  inputTime.min = openingTime;
+  inputTime.max = closingTime;
+});
