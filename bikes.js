ab1bca19755fcfbf5765baea10918a0897845e1e
jasin1 <jasin.tairaidrissi@gmail.com>
2023-01-03 12:49:04 +0100

    ff niet leeg

--- bikes.js
+++ bikes.js
@@ -496,7 +496,7 @@ const fp = flatpickr("#input-date", {
 
     // Clear the current options in the #input-time dropdown
     let inputTime = document.querySelector("#input-time");
-    inputTime.innerHTML = "";
+    //inputTime.innerHTML = "";
 
     // Add the available times as options in the #input-time dropdown
     availableTimes.forEach((time) => {
