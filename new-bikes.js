034850b406d8101774462689485f09540d7b5b3f
jasin1 <jasin.tairaidrissi@gmail.com>
2023-01-05 17:05:55 +0100

    collected time match selected time

--- new-bikes.js
+++ new-bikes.js
@@ -506,5 +506,6 @@ flatpickr("#input-date",  {
     });
     //console.log(inputTime.value);
     step2Time.innerHTML = inputTime.value;
+    timeCollected.setAttribute("value", inputTime.value);
   }
 });
