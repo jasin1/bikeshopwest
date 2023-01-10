ae1c9231f3edb49230d75332787dcddee5ad4ef7
jasin1 <jasin.tairaidrissi@gmail.com>
2023-01-10 07:37:04 +0100

    adding selected date to the mix

--- new-bikes.js
+++ new-bikes.js
@@ -491,6 +491,7 @@ flatpickr("#input-date",  {
   onChange: function(selectedDates, dateStr, instance) {
     // Update the stepTwoDate element with the selected date
     stepTwoDate.innerHTML = dateStr;
+    selectedDate.innerHTML = dateStr;
     // Get the day of the week for the selected date
     let dayOfWeek = new Date(dateStr).toLocaleString("en-US", { weekday: "long" });
 
