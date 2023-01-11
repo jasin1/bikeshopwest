e4f51d37e35a7797d624575244362991aece9a29
jasin1 <jasin.tairaidrissi@gmail.com>
2023-01-11 09:00:44 +0100

    empty input time

--- new-bikes.js
+++ new-bikes.js
@@ -509,7 +509,7 @@ flatpickr("#input-date", {
 
     // Clear the current options in the #input-time dropdown
     //let inputTime = document.querySelector("#input-time");
-    //inputTime.innerHTML = "";
+    inputTime.innerHTML = "";
 
     // Add the available times as options in the #input-time dropdown
     availableTimes.forEach((time) => {
