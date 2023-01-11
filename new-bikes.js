497370781b1db51fd661158bd28a3268557dbe63
jasin1 <jasin.tairaidrissi@gmail.com>
2023-01-11 08:10:27 +0100

    fuck you bitch

--- new-bikes.js
+++ new-bikes.js
@@ -497,7 +497,7 @@ flatpickr("#input-date", {
 
     // Clear the current options in the #input-time dropdown
     let inputTime = document.querySelector("#input-time");
-    inputTime.innerHTML = "";
+    inputTime.innerHTML = "something";
 
     // Add the available times as options in the #input-time dropdown
     availableTimes.forEach((time) => {
