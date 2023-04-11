0318596af4d2811fe3f71b7a95c2689890e03ff9
jasin1 <jasin.tairaidrissi@gmail.com>
2023-04-11 11:39:23 +0200

    Beginning Logic

--- rad-service.js
+++ rad-service.js
@@ -1,1 +1,13 @@
 console.log("this is rad service");
+
+//------- selectors ------------//
+
+const radDescription = document.querySelector(".rad-step-description");
+
+const radForm = {
+  currentStep: 1,
+  radSteps: document.querySelectorAll(".rad-selection"),
+  // nextButtons: document.querySelectorAll('[id^="next"]'),
+  prevButtons: document.querySelectorAll(".prev-button"),
+  submitButton: document.getElementById("rad-submit"),
+};
