2948c3c96f18d6bd0cd0e00cdc4b235d76f9eac7
jasin1 <jasin.tairaidrissi@gmail.com>
2023-04-12 08:02:09 +0200

    flatpickr init

--- rad-service.js
+++ rad-service.js
@@ -4,10 +4,47 @@ console.log("this is rad service");
 
 const radDescription = document.querySelector(".rad-step-description");
 
-const radForm = {
-  currentStep: 1,
-  radSteps: document.querySelectorAll(".rad-selection"),
-  // nextButtons: document.querySelectorAll('[id^="next"]'),
-  prevButtons: document.querySelectorAll(".prev-button"),
-  submitButton: document.getElementById("rad-submit"),
+const serviceButton = document.getElementById("rad-service-btn");
+const testButton = document.getElementById("rad-test-btn");
+const calendarInput = document.getElementById("rad-calendar");
+const radFormFields = document.querySelectorAll(".form-field-wrapper");
+const backButton = document.querySelectorAll(".rad-back-btn");
+
+let radCurrentStep = 0;
+let radSelection = "";
+
+serviceButton.addEventListener("click", function(){
+  showStep(1);
+  radSelection = "service";
+});
+
+testButton.addEventListener("click", function(){
+  showStep(1);
+  radSelection = "test-ride";
+});
+
+backButton.addEventListener("click",function(){
+  showStep(radCurrentStep - 1);
+});
+
+
+function showStep(stepIndex){
+  radSelection[radCurrentStep].classList.remove("active");
+  radSelection[stepIndex].classList.add("active");
 };
+
+
+//------------ flatpickr --------------------//
+flatpickr(calendarInput,{
+  mode: "multiple",
+  enableTime: false,
+  dateFormat: "d-m-Y",
+  minDate: "today",
+  inline: true,
+  onChange: function(selectedDates, dateStr, instance){
+
+  }
+});
+
+
+
