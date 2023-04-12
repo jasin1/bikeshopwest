4db12eef2c1ab6e8d3e899dcd0544729f6fe4969
jasin1 <jasin.tairaidrissi@gmail.com>
2023-04-12 08:29:22 +0200

    wrapping flatpickr inside doc eventListener

--- rad-service.js
+++ rad-service.js
@@ -6,7 +6,7 @@ const radDescription = document.querySelector(".rad-step-description");
 
 const serviceButton = document.getElementById("rad-service-btn");
 const testButton = document.getElementById("rad-test-btn");
-const calendarInput = document.getElementById("rad-calendar");
+//const calendarInput = document.getElementById("rad-calendar");
 const radFormFields = document.querySelectorAll(".form-field-wrapper");
 const backButton = document.querySelectorAll(".rad-back-btn");
 
@@ -53,15 +53,19 @@ const testTimes = {
   Sunday: { open: "", close: "" },
 }
 
-flatpickr(calendarInput,{
-  mode: "multiple",
-  enableTime: false,
-  dateFormat: "d-m-Y",
-  minDate: "today",
-  inline: true,
-  onChange: function(selectedDates, dateStr, instance){
+document.addEventListener('DOMContentLoaded', function() {
+  const calendarInput = document.getElementById("rad-calendar");
 
-  }
+  flatpickr(calendarInput, {
+    mode: "multiple",
+    enableTime: false,
+    dateFormat: "d-m-Y",
+    minDate: "today",
+    inline: true,
+    onChange: function(selectedDates, dateStr, instance) {
+      // Code that updates time slots based on selected date goes here
+    }
+  });
 });
 
 
