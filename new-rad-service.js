8e6ced26c2f522c7c50c0a7345be02d71fc85001
jasin1 <jasin.tairaidrissi@gmail.com>
2023-04-16 19:01:23 +0200

    place everything inside window

--- new-rad-service.js
+++ new-rad-service.js
@@ -1,58 +1,60 @@
 console.log("Rad start over");
 
-const radDescription = document.querySelector(".rad-step-description");
-const serviceButton = document.getElementById("rad-service-btn");
-const testButton = document.getElementById("rad-test-btn");
-const radFormFields = document.querySelectorAll(".form-field-wrapper");
-const backButton = document.querySelectorAll(".rad-back-btn");
-const timeSlotsWrapper = document.querySelector(".rad-timeslots-wrapper");
-const calendarInput = document.getElementById("rad-calendar");
+window.addEventListener("load", function () {
+  const radDescription = document.querySelector(".rad-step-description");
+  const serviceButton = document.getElementById("rad-service-btn");
+  const testButton = document.getElementById("rad-test-btn");
+  const radFormFields = document.querySelectorAll(".form-field-wrapper");
+  const backButton = document.querySelectorAll(".rad-back-btn");
+  const timeSlotsWrapper = document.querySelector(".rad-timeslots-wrapper");
+  const calendarInput = document.getElementById("rad-calendar");
 
-const radOpeningTimes = {
-  Monday: { open: "14:00", close: "17:45" },
-  Tuesday: { open: "10:00", close: "17:45" },
-  Wednesday: { open: "10:00", close: "17:45" },
-  Thursday: { open: "10:00", close: "17:45" },
-  Friday: { open: "15:00", close: "17:45" },
-  Saturday: { open: "10:00", close: "16:45" },
-  Sunday: { open: "", close: "" },
-};
-const testTimes = {
-  Tuesday: { open: "12:00", close: "14:00" },
-  Thursday: { open: "12:00", close: "14:00" },
-  Saturday: { open: "11:00", close: "15:00" },
-  Sunday: { open: "", close: "" },
-  Monday: { open: "", close: "" },
-  Wednesday: { open: "", close: "" },
-  Friday: { open: "", close: "" },
-};
+  const radOpeningTimes = {
+    Monday: { open: "14:00", close: "17:45" },
+    Tuesday: { open: "10:00", close: "17:45" },
+    Wednesday: { open: "10:00", close: "17:45" },
+    Thursday: { open: "10:00", close: "17:45" },
+    Friday: { open: "15:00", close: "17:45" },
+    Saturday: { open: "10:00", close: "16:45" },
+    Sunday: { open: "", close: "" },
+  };
+  const testTimes = {
+    Tuesday: { open: "12:00", close: "14:00" },
+    Thursday: { open: "12:00", close: "14:00" },
+    Saturday: { open: "11:00", close: "15:00" },
+    Sunday: { open: "", close: "" },
+    Monday: { open: "", close: "" },
+    Wednesday: { open: "", close: "" },
+    Friday: { open: "", close: "" },
+  };
 
-//------------- make a choice in step 1 ---------------//
+  //------------- make a choice in step 1 ---------------//
 
-let notTheseDays = {
-  service: ["Sunday"],
-  test: ["Sunday", "Monday", "Wednesday", "Friday"],
-};
+  let notTheseDays = {
+    service: ["Sunday"],
+    test: ["Sunday", "Monday", "Wednesday", "Friday"],
+  };
 
-const radFlatP = flatpickr(calendarInput, {
-  enableTimes: false,
-  dateFormat: "d.m.Y",
-  minDate: "today",
-  inline: true,
-  maxDate: new Date().fp_incr(90),
-  disable: [],
-});
+  const radFlatP = flatpickr(calendarInput, {
+    enableTimes: false,
+    dateFormat: "d.m.Y",
+    minDate: "today",
+    inline: true,
+    maxDate: new Date().fp_incr(90),
+    disable: [],
+  });
 
-serviceButton.addEventListener("click", function () {
-  console.log("service btn clicked");
-  serviceButton.classList.add("active");
-  testButton.classList.remove("active");
-  radFlatP.set("disable", notTheseDays.service);
-});
+  serviceButton.addEventListener("click", function () {
+    console.log("service btn clicked");
+    serviceButton.classList.add("active");
+    testButton.classList.remove("active");
+    radFlatP.set("disable", notTheseDays.service);
+  });
 
-testButton.addEventListener("click", function () {
-  console.log("test btn clicked");
-  testButton.classList.add("active");
-  serviceButton.classList.remove("active");
-  radFlatP.set("disable", notTheseDays.test);
+  testButton.addEventListener("click", function () {
+    console.log("test btn clicked");
+    testButton.classList.add("active");
+    serviceButton.classList.remove("active");
+    radFlatP.set("disable", notTheseDays.test);
+  });
 });
